b1a47a485b8e0e881376639a77fb4a829a5d4707
Jaap Wijnen <jaapwijnen@Jaaps-MacBook-Pro.local>
2017-06-06 22:16:07 -0700

    Initial Commit

---
+++ Fissa/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Fissa
+//
+//  Created by Jaap Wijnen on 06/06/2017.
+//  Copyright © 2017 Workmoose. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Fissa/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Fissa
+//
+//  Created by Jaap Wijnen on 06/06/2017.
+//  Copyright © 2017 Workmoose. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
